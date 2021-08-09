5953de649a42d56dd4baf413440fc72a5097aa6a
crane <xyyh0116@aliyun.com>
2021-08-09 17:42:46 +0800

    update

--- md_web_flutter/web/flutter_service_worker.js
+++ md_web_flutter/web/flutter_service_worker.js
@@ -4,18 +4,22 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "cd86acc801d1f35fc6e5e1a9a0c34549",
-"index.html": "808778304b93aa5d80dbb70f24a00922",
-"/": "808778304b93aa5d80dbb70f24a00922",
-"main.dart.js": "adc603b5110a46d8c2a7ed76b06a36e4",
+"index.html": "f9ee886dbb00cd45902adebdc83b9af1",
+"/": "f9ee886dbb00cd45902adebdc83b9af1",
+"main.dart.js": "6a558f3deffb6eb3bae79d29202815a0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "97d63907ba6d7e6de2af93fa73aa1225",
-"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/NOTICES": "bde9ee889e7cdb2c908965b81d635c52",
+"assets/AssetManifest.json": "685c115d84dcc4a2f38eee906a38b681",
+"assets/NOTICES": "7fd9a5ae027c3e3741d25730dd4bd0c0",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1"
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"assets/assets/images/default_1x1.png": "52aec2450944eff316440f1312886489",
+"assets/assets/images/empty_project.png": "35df4f7c11ca618a70ea778eb7ea4422",
+"assets/assets/images/ic_header_refresh.gif": "450889627f39c0e9c17811eb2c5b55c7",
+"assets/assets/images/modian_logo.png": "78569dde6e88471e8322fe1dc7a1b29d"
 };
 
 // The application shell files that are downloaded before a service worker can
